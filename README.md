f9eae57a30934424625e5a18ea9e4c2a229bec8f
Romulo Vitor <romulolemos.altran@sulamerica.com.br>
2022-01-07 12:40:35 -0300

    Test and readme

--- README.md
+++ README.md
@@ -1,24 +1,19 @@
-Overview
+#Overview
 
 This repository contains the scripts corresponding to the test proposed by Cocus.
 
-Prerequisites
+###Prerequisites
 
     Python >= 3.7.1
     Virtualenv >= 20.0.35
 
-Deploy
+###Deploy
     
     1 - Clone this repository ( '$ git clone https://github.com/romulovitor/cocus_assignment.git ' )
-    
     2 - Get in the project ( '$ cd cocus_assignment ')
-    
-    3 - Run to create the environment of development ('$ make install ')
-    
-    4 - Activate the virtual dependencies ('$ source venv/bin/activate ')
-    
-    5 - Run script ('$ python <scritp_name> ')
-    
-    6 - Run test ('$ pytest tests ')
-
-    7 - Run coverage ('$ coverage run -m pytest ')
+    3 - Create venv using Makefile ('$ make venv ')
+    4 - Activate the virtual dependencies ('$ source venv/bin/activate ')    
+    5 - Run to create the environment of development ('$ make install ')
+    6 - Run script ('$ python <scritp_name> ')
+    7 - Run test ('$ pytest tests ') # Incomplete
+    8 - Run coverage ('$ coverage run -m pytest ')
--- tests/test_find_sufix.py
+++ tests/test_find_sufix.py
@@ -1,30 +1,21 @@
+class TestClass(object):  # pragma: no cover
+    def test_find_rigth_path(self):
+        retorno = find_by_sufix('/home/romulo/dev/gitlab/cocus_assignment', '.py')
+        assert retorno is not None
 
-def find_by_sufix(suffix, path):
+    def test_find_rigth_wrong_path(self):
+        retorno = find_by_sufix('/home/romulo/dev/gitlab/cocus_assignment', '.csv')
+        assert retorno is None
+
+
+def find_by_sufix(path, suffix):
     import os
-    """
-    Find all files within a path, with a given file name suffix. Note that a path may contain further
-    subdirectories and those subdirectories may also contain further subdirectories. There is no limit
-    to the depth of the subdirectories.
-    Arguments:
-        suffix(str): suffix if the file name to be found
-        path(str): path of the file system
-    Returns:
-        a list of paths and file names that match the suffix
-    """
-    path_list = []
+    path_list = []   # pragma: no cover
     for root, dirs, files in os.walk(path):
         for file in files:
             if (file.endswith(suffix)):
-                # print(os.path.join(root, file))
                 path_list.append(os.path.join(root, file))
-    return path_list
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    path = "/home/romulo/dev/gitlab/cocus_assignment"
-    suffix = ".py"
-    # print(find_by_sufix(suffix, path))
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if len(path_list) > 0:  # pragma: no cover
+        return path_list
+    else:
+        return None  # pragma: no cover
