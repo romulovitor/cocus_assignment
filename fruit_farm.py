34ce9526f5119f3aedabafd1f19a266aaf65634d
Romulo Vitor <romulolemos.altran@sulamerica.com.br>
2022-01-11 03:31:09 -0300

    Adjust in the function logging

--- fruit_farm.py
+++ fruit_farm.py
@@ -1,3 +1,4 @@
+import datetime
 import threading
 import time
 import logging
@@ -5,16 +6,18 @@ import random
 import queue
 
 logging.basicConfig(level=logging.DEBUG,
-                    format='(%(threadName)-9s) %(message)s', )
+                    format=' %(message)s', )
 
 BUF_SIZE = 50
 dirty_basket = queue.Queue(BUF_SIZE)
 clean_basket = queue.Queue(BUF_SIZE)
+queue_log = queue.Queue(BUF_SIZE + BUF_SIZE)
 tot_fruit = 50
 
 
 def producer(id):
     global tot_fruit
+
     while tot_fruit > 0:
 
         if not dirty_basket.full():
@@ -22,9 +25,8 @@ def producer(id):
             tot_fruit -= 1
             item = tot_fruit
             dirty_basket.put(item)
+            queue_log.put(f'Farmer-{id} ({item})')
             threadLock.release()
-            logging.debug(f'Farmer-{id} ' + f'({item})'
-                          + ' : ' + str(dirty_basket.qsize()) + ' Fruit in the dirty basket')
             dalay_wait = random.randint(3, 6)
             time.sleep(dalay_wait)
     return
@@ -36,12 +38,28 @@ def consumer(id):
         if not dirty_basket.empty():
             item = dirty_basket.get()
             clean_basket.put(item)
-            logging.debug(f'Cleaner-{id} ' + f'({item})'
-                          + ' : ' + str(clean_basket.qsize()) + ' Fruit in the clean basket')
-            # logging.debug('Clean basket ' + str(item)
-            #               + ' : ' + str(clean_basket.qsize()) + ' items in queue')
+            queue_log.put(f'Cleaner-{id} ({item})')
             dalay_wait = random.randint(2, 4)
             time.sleep(dalay_wait)
+    return
+
+
+def logging_app():
+    """
+    function to monitor logs displayed by producer and consumer
+    :return:
+    """
+    while tot_fruit > 0:
+        time.sleep(1)
+        # if str(queue_log.get).startswith("Farmer"):
+        # print(queue_log.get())
+        logging.debug(
+            f' {datetime.datetime.now().replace(microsecond=0)} Tree ({tot_fruit} fruits) - dirty basket ({dirty_basket.qsize()}) '
+            f'- Clean Basket ( {clean_basket.qsize()} ) -  '
+            f'{queue_log.get()} - '
+            f'{queue_log.get()}'
+
+            )
     return
 
 
@@ -50,16 +68,20 @@ if __name__ == '__main__':
     threads = []
 
     for i in range(3):
-        p = threading.Thread(target=producer, args=(i,))
+        p = threading.Thread(target=producer, args=(i,), name=f'Farmer-{i}')
         p.start()
 
         # time.sleep(2)
-        c = threading.Thread(target=consumer, args=(i,))
+        c = threading.Thread(target=consumer, args=(i,), name=f'Cleaner-{i}')
         c.start()
 
         threads.append(p)
         threads.append(c)
 
+    log = threading.Thread(target=logging_app, name=f'Logging')
+    log.start()
+
+    threads.append(log)
     # Wait for all threads to complete
     for t in threads:
         t.join()
