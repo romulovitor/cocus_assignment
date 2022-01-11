dbb82aa381829c1f5c4b2fbf758177b29916fada
Romulo Vitor <romulolemos.altran@sulamerica.com.br>
2022-01-11 03:31:28 -0300

    It was add new tests

--- tests/test_find_sufix.py
+++ tests/test_find_sufix.py
@@ -1,21 +1,25 @@
-class TestClass(object):  # pragma: no cover
+# pragma: no cover
+import unittest
+
+
+class TestClass(unittest.TestCase):
     def test_find_rigth_path(self):
         retorno = find_by_sufix('/home/romulo/dev/gitlab/cocus_assignment', '.py')
-        assert retorno is not None
+        self.assertNotEqual(retorno,None)
 
     def test_find_rigth_wrong_path(self):
         retorno = find_by_sufix('/home/romulo/dev/gitlab/cocus_assignment', '.csv')
-        assert retorno is None
+        self.assertEqual(retorno,None)
 
 
 def find_by_sufix(path, suffix):
     import os
-    path_list = []   # pragma: no cover
+    path_list = []
     for root, dirs, files in os.walk(path):
         for file in files:
-            if (file.endswith(suffix)):
+            if file.endswith(suffix):
                 path_list.append(os.path.join(root, file))
-    if len(path_list) > 0:  # pragma: no cover
+    if len(path_list) > 0:
         return path_list
     else:
-        return None  # pragma: no cover
+        return None
--- tests/test_fruit_farm.py
+++ tests/test_fruit_farm.py
@@ -12,21 +12,45 @@ dirty_basket = queue.Queue(BUF_SIZE)
 clean_basket = queue.Queue(BUF_SIZE)
 tot_fruit = 3
 
+import unittest
+class TestClass(unittest.TestCase):
 
-class TestClass(object):
-
-    def test_find_rigth_path(self):
-        # retorno = producer(1)
-        # assert retorno is not None
-        pass
-
-    def test_find_rigth_path(self):
-        # retorno = consumer(1)
-        # assert retorno is not None
-        pass
+    def test_name_producer(self):
+        p = threading.Thread(target=producer, name='Cleaner-1')
+        p.start()
+        self.assertEqual("Cleaner-1", p.getName(), "Nome diferente do esperado")
 
 
-def producer(id):
+    def test_name_consumer(self):
+        c = threading.Thread(target=consumer, name='Cleaner-1')
+        c.start()
+        self.assertEqual("Cleaner-1", c.getName(), "Nome diferente do esperado")
+
+
+    def test_consumer_alive(self):
+        c = threading.Thread(target=consumer, name='Cleaner-1')
+        c.start()
+        self.assertTrue(c.is_alive(), "Consumer alive to read from queue")
+
+
+    def test_extract_from_tree_failed(self):
+        global tot_fruit
+        tot_fruit = -1
+        p = threading.Thread(target=producer, name='Farmer-1')
+        p.start()
+        self.assertEqual(p.is_alive(), False, "Não deve ser ativa pelo fato de não ter frutas (tot_fruit = -1) ")
+
+    def test_extract_from_tree(self):
+        global tot_fruit
+        tot_fruit = 50
+        p = threading.Thread(target=producer, name='Farmer-1')
+        p.start()
+        self.assertTrue(p, "Deve ser ativada pelo fato de possuir frutas ")
+
+
+
+def producer():
+    threadLock = threading.Lock()
     global tot_fruit
     while tot_fruit > 0:
 
@@ -40,12 +64,22 @@ def producer(id):
                           + ' : ' + str(dirty_basket.qsize()) + ' Fruit in the dirty basket')
             dalay_wait = random.randint(3, 6)
             time.sleep(dalay_wait)
-            return
+            return 1
 
     return None
 
+def logging_app(c,p):
+    while tot_fruit > 0:
+        time.sleep(1)
+        logging.debug(f' Tree ({tot_fruit} fruits) - dirty basket ({dirty_basket.qsize()}) '
+                      f'- Clean Basket ( {clean_basket.qsize()} ) -  '
+                      f'{p.getName()} ({clean_basket.get()}) - '
+                             f'{c.getName()} ({clean_basket.get()})')
+        return True
+    return
 
-def consumer(id):
+
+def consumer():
     global tot_fruit
     while tot_fruit > 0:
         if not dirty_basket.empty():
@@ -62,20 +96,7 @@ def consumer(id):
 
 
 if __name__ == '__main__':
+    unittest.main()
+
     threadLock = threading.Lock()
     threads = []
-
-    p = threading.Thread(target=producer, args=(1,))
-    p.start()
-
-    # time.sleep(2)
-    c = threading.Thread(target=consumer, args=(1,))
-    c.start()
-
-    threads.append(p)
-    threads.append(c)
-
-    # Wait for all threads to complete
-    for t in threads:
-        t.join()
-    print("Exiting Main Thread")
